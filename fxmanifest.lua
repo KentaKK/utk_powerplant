55a3ae8ba1ae46f4043c48281a6d17898f5021c0
Kenta <102387086+KentaKK@users.noreply.github.com>
2023-11-04 19:05:00 +0100

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,16 +1,1 @@
-fx_version 'cerulean'game "gta5"
-lua54 'yes'
-use_experimental_fxv2_oal 'yes'
-
-shared_script '@es_extended/imports.lua'
-
-
-server_scripts {
-    'server.lua',
-    'config.lua'
-}
-
-client_scripts {
-    'client/powerplant.lua',
-    'config.lua'
-}
+0
